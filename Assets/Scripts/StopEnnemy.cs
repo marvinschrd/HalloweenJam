2664c5e2192d2944cd72e00af10ad0435442648d
William <williamp10022000@gmail.com>
2020-11-07 20:59:52 +0100

    Added stop chase

---
+++ Assets/Scripts/StopEnnemy.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Transactions;
+using Pathfinding;
+using UnityEngine;
+
+public class StopEnnemy : MonoBehaviour
+{
+    AILerp AILerp;
+    [SerializeField] Transform BidenTrans;
+    Transform EnnemyTrans;
+    void Start()
+    {
+        EnnemyTrans = GetComponent<Transform>();
+        AILerp = GetComponent<AILerp>();
+    }
+
+    private void Update()
+    {
+        if (Vector2.Distance(BidenTrans.position, EnnemyTrans.position) < 1)
+        {
+            AILerp.enabled = (false);
+        }
+        else
+        {
+            AILerp.enabled = (true);
+        }
+    }
+}
