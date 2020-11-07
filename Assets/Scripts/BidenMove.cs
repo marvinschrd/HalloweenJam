be57e1bcd49602b471250bed265befd7dd769638
William <williamp10022000@gmail.com>
2020-11-07 15:24:11 +0100

    Biden

---
+++ Assets/Scripts/BidenMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BidenMove : MonoBehaviour
+{
+    
+    Rigidbody2D bidenBody;
+    Transform bidenPos;
+    Vector2 bidenDirection;
+    [SerializeField] float speed;
+    
+    void Start()
+    {
+        bidenBody = GetComponent<Rigidbody2D>();
+        bidenPos = GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        bidenDirection = new Vector2( Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * speed;
+        bidenBody.velocity = bidenDirection;
+        bidenPos.rotation.z = 0;
+    }
+}
