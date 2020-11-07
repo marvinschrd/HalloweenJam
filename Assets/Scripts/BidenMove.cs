af0a2c3f4d3ade0d0f0b64ee9fa15ab1989aef77
William <williamp10022000@gmail.com>
2020-11-07 15:44:37 +0100

    Test Biden

--- Assets/Scripts/BidenMove.cs
+++ Assets/Scripts/BidenMove.cs
@@ -6,20 +6,17 @@ public class BidenMove : MonoBehaviour
 {
     
     Rigidbody2D bidenBody;
-    Transform bidenPos;
     Vector2 bidenDirection;
     [SerializeField] float speed;
     
     void Start()
     {
         bidenBody = GetComponent<Rigidbody2D>();
-        bidenPos = GetComponent<Transform>();
     }
 
     void Update()
     {
         bidenDirection = new Vector2( Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * speed;
         bidenBody.velocity = bidenDirection;
-        //bidenPos.rotation.z = 0;
     }
 }
